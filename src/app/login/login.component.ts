308823e4886c178c8d82af434bdaad3985116bdf
XaviAlvarado18 <alv21188@uvg.edu.gt>
2024-07-25 22:21:19 -0600

    xmpp client: day one

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,34 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule} from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  ngOnInit(): void {}
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      const email = this.loginForm.get('email')?.value;
+      const password = this.loginForm.get('password')?.value;
+      console.log('Email:', email);
+      console.log('Password:', password);
+      // Aquí puedes llamar a tu servicio de autenticación
+    }
+  }
+}
