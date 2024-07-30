df194cf0b1e910decd74de0e83cedb022536b249
XaviAlvarado18 <alv21188@uvg.edu.gt>
2024-07-29 18:03:51 -0600

    Lateral bar component

--- src/app/chat-main/chat-main.component.ts
+++ src/app/chat-main/chat-main.component.ts
@@ -1,27 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { ImageButtonComponent } from '../image-button/image-button.component';
+import { LateralBarComponent } from '../lateral-bar/lateral-bar.component';
 
 
 @Component({
   selector: 'app-chat-main',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, ImageButtonComponent],
+  imports: [RouterOutlet, CommonModule, LateralBarComponent],
   templateUrl: './chat-main.component.html',
   styleUrl: './chat-main.component.scss'
 })
 export class ChatMainComponent {
 
-  pathToImage: string = 'assets/no_avatar.svg';
-
-
-  onItemClick(item: any) {
-    // Manejar el clic en los elementos de la barra lateral
-    console.log('Clicked on:', item.name);
-  }
-
-  handleClick() {
-    console.log('Button clicked!');
-  }
+  
 }
---
+++ src/app/lateral-bar/lateral-bar.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { ImageButtonComponent } from '../image-button/image-button.component';
+
+@Component({
+  selector: 'app-lateral-bar',
+  standalone: true,
+  imports: [ImageButtonComponent],
+  templateUrl: './lateral-bar.component.html',
+  styleUrl: './lateral-bar.component.scss'
+})
+export class LateralBarComponent {
+
+  pathToImage: string = 'assets/no_avatar.svg';
+
+
+  onItemClick(item: any) {
+    // Manejar el clic en los elementos de la barra lateral
+    console.log('Clicked on:', item.name);
+  }
+
+  handleClick() {
+    console.log('Button clicked!');
+  }
+
+}
